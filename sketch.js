11834e31697a5cd985acd5b14107450a3e8af038
Kartik133 <75470483+Kartik133@users.noreply.github.com>
2022-04-28 18:57:25 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,21 +1,18 @@
-var speech,a,b;
+var voice,a,b;
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
   
-  speech = new p5.Speech(voiceReady);
+  voice = new p5.Speech();
   a = createInput("");
   a.position(width/2-30,height/2);
   b = createButton("Speak");
   b.position(width/2-30,height/2+30);
-  function voiceReady(){
-    console.log(speech.voices);
-  }
 }
 
 function draw() {
   background(0);
   b.mouseReleased(()=> {
-    speech.speak(a.value());
+    voice.speak(a.value());
   });
 }
