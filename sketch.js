0acbdefe1607a5c35b1ddc133591d2f4074ec1c0
Kartik133 <75470483+Kartik133@users.noreply.github.com>
2022-05-15 13:51:24 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,17 +1,21 @@
-var voice,a,b;
+var voice,a,b,sel,c;
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
   
   voice = new p5.Speech();
   a = createInput("");
-  a.position(width/2-30,height/2);
+  a.position(width/2-88.5,height/2);
   b = createButton("Speak");
-  b.position(width/2-30,height/2+30);
+  b.position(width/2-88.5,height/2+60);
+
+  voice.setRate("0.9");
+  voice.setVoice("Cellos");
 }
 
 function draw() {
   background(0);
+
   b.mouseReleased(()=> {
     voice.speak(a.value());
   });
